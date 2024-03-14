0c8ff948398f91804cb135b2e9dbb20fde078093
Christophe Lambin <christophe.lambin@gmail.com>
2024-03-14 13:06:08 +0100

    refactor: monitor

--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -13,13 +13,17 @@ import (
 )
 
 func TestMonitor(t *testing.T) {
+	h := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	metrics := monitor.NewHTTPMetrics("uptime", "monitor")
 	assert.NoError(t, testutil.CollectAndCompare(metrics, bytes.NewBufferString(``)))
 
 	m := monitor.New(metrics, nil)
 
-	req := monitor.Request{Target: "http://localhost", Interval: 10 * time.Millisecond}
-	r, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/target?"+req.Encode(), nil)
+	req := monitor.Request{Target: h.URL, Interval: 10 * time.Millisecond}
+	r, _ := http.NewRequest(http.MethodPost, "/target?"+req.Encode(), nil)
 	w := httptest.NewRecorder()
 
 	m.ServeHTTP(w, r)
@@ -32,6 +36,34 @@ func TestMonitor(t *testing.T) {
 	assert.NoError(t, testutil.CollectAndCompare(metrics, bytes.NewBufferString(`
 # HELP uptime_monitor_up site is up/down
 # TYPE uptime_monitor_up gauge
-uptime_monitor_up{host="http://localhost"} 0
+uptime_monitor_up{host="`+h.URL+`"} 1
 `), "uptime_monitor_up"))
+
+	h.Close()
+
+	assert.Eventually(t, func() bool {
+		return nil == testutil.CollectAndCompare(metrics, bytes.NewBufferString(`
+# HELP uptime_monitor_up site is up/down
+# TYPE uptime_monitor_up gauge
+uptime_monitor_up{host="`+h.URL+`"} 0
+`), "uptime_monitor_up")
+	}, time.Second, 20*time.Millisecond)
+
+	req = monitor.Request{Target: h.URL}
+	r, _ = http.NewRequest(http.MethodDelete, "/target?"+req.Encode(), nil)
+	w = httptest.NewRecorder()
+
+	m.ServeHTTP(w, r)
+	require.Equal(t, http.StatusOK, w.Code)
+
+	// FIXME: deleted targets will continue to be reported on!
+	assert.NoError(t, testutil.CollectAndCompare(metrics, bytes.NewBufferString(`
+# HELP uptime_monitor_up site is up/down
+# TYPE uptime_monitor_up gauge
+uptime_monitor_up{host="`+h.URL+`"} 0
+`), "uptime_monitor_up"))
+
+	//	assert.Eventually(t, func() bool {
+	//		return testutil.CollectAndCount(metrics) == 0
+	//	}, time.Second, 20*time.Millisecond)
 }
